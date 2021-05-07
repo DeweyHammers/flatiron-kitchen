8b5456e653e0489e352a2be76ac62c5261225e21
Dewey <hammersdrichard@gmail.com>
2021-05-07 03:53:52 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   validates :name, presence: true
+  accepts_nested_attributes_for :ingredients, reject_if: proc {|attr| attr['name'].blank? }
 end
